0921fefca0558cb3fcd3db9cfcaa8bba983e22bf
Chantee Smith <chantee.smith@gmail.com>
2019-12-10 22:23:14 -0600

    Pushing up a previous assignment that I didn't create a repository for.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 var counter = { data: 0 }
 
